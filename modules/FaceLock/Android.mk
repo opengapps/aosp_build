0008dd0b7ede29d8f3bce81c31bf0aeb56a786bc
Arne-Christian Blystad <arne.christian@blystad.me>
2015-10-19 17:21:29 +0200

    FaceLock: Add libfacelock & libfrsdk & way to config MODULE_PATH

--- modules/FaceLock/Android.mk
+++ modules/FaceLock/Android.mk
@@ -4,12 +4,22 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := FaceLock
 LOCAL_PACKAGE_NAME := com.android.facelock
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/app
-LOCAL_SHARED_LIBRARIES := libfilterpack_facedetect
+LOCAL_SHARED_LIBRARIES := libfilterpack_facedetect libfacelock_jni libfrsdk
 
 include $(BUILD_GAPPS_PREBUILT_APK)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := libfilterpack_facedetect
 LOCAL_SRC_FILES := $(GAPPS_SOURCES_PATH)/arm/lib/libfilterpack_facedetect.so
+
+include $(BUILD_GAPPS_PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libfacelock_jni
+LOCAL_SRC_FILES := $(GAPPS_SOURCES_PATH)/arm/lib/libfacelock_jni.so
+
+include $(BUILD_GAPPS_PREBUILT_SHARED_LIBRARY)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libfrsdk
+LOCAL_SRC_FILES := $(GAPPS_SOURCES_PATH)/arm/vendor/lib/libfrsdk.so
+GAPPS_MODULE_PATH := TARGET_OUT_VENDOR_SHARED_LIBRARIES
 
 include $(BUILD_GAPPS_PREBUILT_SHARED_LIBRARY)
