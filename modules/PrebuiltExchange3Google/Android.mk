b43bb1d259feeaa3275b2d1a0e34294800b9fdd3
Arne-Christian Blystad <arne.christian@blystad.me>
2015-10-20 18:03:19 +0200

    Exchange: Override Exchange2

--- modules/PrebuiltExchange3Google/Android.mk
+++ modules/PrebuiltExchange3Google/Android.mk
@@ -5,4 +5,6 @@ LOCAL_MODULE := PrebuiltExchange3Google
 LOCAL_PACKAGE_NAME := com.google.android.gm.exchange
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/app
 
+LOCAL_OVERRIDES_PACKAGES := Exchange2
+
 include $(BUILD_GAPPS_PREBUILT_APK)
