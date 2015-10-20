bbf49246080cbf9eecc3a41ffcd707e6e6352498
Arne-Christian Blystad <arne.christian@blystad.me>
2015-10-20 18:03:05 +0200

    Chrome: Overwrite Browser if stock/super

--- modules/Chrome/Android.mk
+++ modules/Chrome/Android.mk
@@ -5,4 +5,8 @@ LOCAL_MODULE := Chrome
 LOCAL_PACKAGE_NAME := com.android.chrome
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/app
 
+ifneq ($(filter $(GAPPS_VARIANT),stock),) # overwrite Browser if stock/super
+LOCAL_OVERRIDES_PACKAGES := Browser
+endif
+
 include $(BUILD_GAPPS_PREBUILT_APK)
