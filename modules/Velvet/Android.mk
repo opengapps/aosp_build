e043b08c02c4df967abc00f6031549e537fb7ea5
Arne-Christian Blystad <arne.christian@blystad.me>
2015-10-22 16:50:14 +0200

    Velvet: Override QuickSearchBox

--- modules/Velvet/Android.mk
+++ modules/Velvet/Android.mk
@@ -5,4 +5,6 @@ LOCAL_MODULE := Velvet
 LOCAL_PACKAGE_NAME := com.google.android.googlequicksearchbox
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/priv-app
 
+LOCAL_OVERRIDES_PACKAGES := QuickSearchBox
+
 include $(BUILD_GAPPS_PREBUILT_APK)
