592d997515f695ec7310ee9243879719facd6a00
Bernd Kuhls <bernd.kuhls@t-online.de>
2015-10-31 12:12:44 +0100

    new package: com.google.android.webview
    Switching from TK GApps to OpenGApps I noticed the Google Webview
    package is not supported by aosp_build although the package itself is
    provided by OpenGApps. Please note that I did not add this package to
    any build variant, I leave the decision to you to decide whether this
    package should be part of the pico build or any other variant.
    
    I did not add this package to any build variant

---
+++ modules/WebViewGoogle/Android.mk
@@ -0,0 +1,8 @@
+# Auto-generated Makefile from OpenGapps. Do not modify.
+LOCAL_PATH := .
+include $(CLEAR_VARS)
+LOCAL_MODULE := WebViewGoogle
+LOCAL_PACKAGE_NAME := com.google.android.webview
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/priv-app
+
+include $(BUILD_GAPPS_PREBUILT_APK)
