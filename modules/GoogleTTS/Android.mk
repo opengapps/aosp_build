e6bde778ac183723f172700c75fa2dc84c01169f
Simone Caronni <negativo17@gmail.com>
2016-01-20 13:15:52 +0100

    Add text-to-speech local override

--- modules/GoogleTTS/Android.mk
+++ modules/GoogleTTS/Android.mk
@@ -4,4 +4,8 @@ LOCAL_MODULE := GoogleTTS
 LOCAL_PACKAGE_NAME := com.google.android.tts
 LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/app
 
+ifneq ($(filter $(TARGET_GAPPS_VARIANT),stock),) # overwrite if stock/super
+LOCAL_OVERRIDES_PACKAGES := PicoTTS
+endif
+
 include $(BUILD_GAPPS_PREBUILT_APK)
