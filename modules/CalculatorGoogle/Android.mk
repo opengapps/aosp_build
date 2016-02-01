617f0a1e1bdd99966271bf3af1d562ec8ed4462d
Simone Caronni <simone.caronni@swisscom.com>
2016-02-01 09:18:24 +0100

    Remove LOCAL_MODULE_PATH

--- modules/CalculatorGoogle/Android.mk
+++ modules/CalculatorGoogle/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := .
 include $(CLEAR_VARS)
 LOCAL_MODULE := CalculatorGoogle
 LOCAL_PACKAGE_NAME := com.google.android.calculator
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/app
 
 ifneq ($(filter $(TARGET_GAPPS_VARIANT),mini),) # overwrite if mini/full/stock/super
 LOCAL_OVERRIDES_PACKAGES := Calculator ExactCalculator
--- modules/TagGoogle/Android.mk
+++ modules/TagGoogle/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := .
 include $(CLEAR_VARS)
 LOCAL_MODULE := TagGoogle
 LOCAL_PACKAGE_NAME := com.google.android.tag
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/system/app
 
 ifneq ($(filter $(TARGET_GAPPS_VARIANT),stock),) # overwrite if stock/super
 LOCAL_OVERRIDES_PACKAGES := Tag
