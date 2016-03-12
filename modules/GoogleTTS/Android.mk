96da03c134454c3e2c40b531cf1502985e0052c5
Cyril Kaiser <cyril.kaiser2@gmail.com>
2016-03-12 03:53:05 +0100

    Fix PicoTTS override (correct package name is PicoTts)

--- modules/GoogleTTS/Android.mk
+++ modules/GoogleTTS/Android.mk
@@ -4,7 +4,7 @@ LOCAL_MODULE := GoogleTTS
 LOCAL_PACKAGE_NAME := com.google.android.tts
 
 ifneq ($(filter $(TARGET_GAPPS_VARIANT),stock),) # overwrite if stock/super
-LOCAL_OVERRIDES_PACKAGES := PicoTTS
+LOCAL_OVERRIDES_PACKAGES := PicoTts
 endif
 
 include $(BUILD_GAPPS_PREBUILT_APK)
