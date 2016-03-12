623b803531fc5ca7c82b72ed4263f611ce4d98bc
Cyril Kaiser <cyril.kaiser2@gmail.com>
2016-03-12 03:55:13 +0100

    Add package overrides for GoogleHome

--- modules/GoogleHome/Android.mk
+++ modules/GoogleHome/Android.mk
@@ -3,4 +3,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := GoogleHome
 LOCAL_PACKAGE_NAME := com.google.android.launcher
 
+ifneq ($(filter $(TARGET_GAPPS_VARIANT),stock),) # overwrite if stock/super
+LOCAL_OVERRIDES_PACKAGES := Launcher2 Launcher3
+endif
+
 include $(BUILD_GAPPS_PREBUILT_APK)
