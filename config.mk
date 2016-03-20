251f57711153b5cd78717d741dfabcc42e41efa8
Cyril Kaiser <cyril.kaiser2@gmail.com>
2016-03-20 22:24:38 +0100

    Minor code refactoring.
    Quote all shell variables, except when required not to (eg: after find -L)
    Trim spaces in makefile function arguments to maintain compatibility with shell commands while maintaining a consistent code style.
    Use consistent indentation.

--- config.mk
+++ config.mk
@@ -26,12 +26,12 @@ include $(GAPPS_BUILD_SYSTEM_PATH)/definitions.mk
 
 # Device should define their GAPPS_VARIANT in device/manufacturer/product/BoardConfig.mk
 ifeq ($(GAPPS_VARIANT),)
-	$(error GAPPS_VARIANT must be configured)
+  $(error GAPPS_VARIANT must be configured)
 endif
 GAPPS_VARIANT_EVAL := $(call get-gapps-variant,$(GAPPS_VARIANT))
 
 ifeq ($(GAPPS_VARIANT_EVAL),)
-$(error GAPPS_VARIANT $(GAPPS_VARIANT) was not found. Use of one of pico,nano,micro,mini,full,stock,super)
+  $(error GAPPS_VARIANT $(GAPPS_VARIANT) was not found. Use of one of pico,nano,micro,mini,full,stock,super)
 endif
 
 TARGET_GAPPS_VARIANT := $(GAPPS_VARIANT_EVAL)
--- opengapps-files.mk
+++ opengapps-files.mk
@@ -1,7 +1,7 @@
 # Always needed
-PRODUCT_COPY_FILES += $(call gapps-copy-to-system, all, etc framework)
+PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,etc framework)
 
 # Pico and higher
 ifneq ($(filter $(TARGET_GAPPS_VARIANT),pico),)
-PRODUCT_COPY_FILES += $(call gapps-copy-to-system, all, vendor/pittpatt usr/srec)
+  PRODUCT_COPY_FILES += $(call gapps-copy-to-system,all,vendor/pittpatt usr/srec)
 endif
