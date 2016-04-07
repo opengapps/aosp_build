eabdbba890ea0699bbc1be2a8c4f498dc58ac95e
Arne-Christian Blystad <arne.christian.blystad@bartec-pixavi.com>
2016-04-07 11:39:49 +0200

    Fix SetupWizard package name is updated based on tablet/phone
    This fixes issue #35

--- modules/SetupWizard/Android.mk
+++ modules/SetupWizard/Android.mk
@@ -2,7 +2,12 @@ LOCAL_PATH := .
 include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := SetupWizard
-LOCAL_PACKAGE_NAME := com.google.android.setupwizard
+ifneq ($(filter $(PRODUCT_CHARACTERISTICS),tablet),)
+	LOCAL_PACKAGE_NAME := com.google.android.setupwizard.tablet
+else
+	LOCAL_PACKAGE_NAME := com.google.android.setupwizard.default
+endif
+
 LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_GAPPS_PREBUILT_APK)
