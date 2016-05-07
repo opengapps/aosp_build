17ac32266c42847fa9bb6f3770354449f0f3221b
Adriano Martins <adrianomartins@gmail.com>
2016-05-07 04:01:35 -0300

    Revert "Remove PrebuiltExchange3Google"
    This reverts commit b90e42365fc7a39b3e8026bb8355a5e6ebe9a2f1.

---
+++ modules/PrebuiltExchange3Google/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := .
+include $(CLEAR_VARS)
+include $(GAPPS_CLEAR_VARS)
+LOCAL_MODULE := PrebuiltExchange3Google
+LOCAL_PACKAGE_NAME := com.google.android.gm.exchange
+
+LOCAL_OVERRIDES_PACKAGES := Exchange2
+
+include $(BUILD_GAPPS_PREBUILT_APK)
