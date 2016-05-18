361f119b514e36de59ad6545a46b11607b7b2076
Arne-Christian Blystad <arne.christian@blystad.me>
2016-05-18 10:39:09 +0200

    PrebuiltExchange3Google: Change to non-privileged

--- modules/PrebuiltExchange3Google/Android.mk
+++ modules/PrebuiltExchange3Google/Android.mk
@@ -3,7 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := PrebuiltExchange3Google
 LOCAL_PACKAGE_NAME := com.google.android.gm.exchange
-LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_OVERRIDES_PACKAGES := Exchange2
 
