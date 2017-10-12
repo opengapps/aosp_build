515d2ee48b82e6e8e5864488b37757a0e016fa10
Arseniy Graur <argraur12@yandex.ru>
2017-10-12 15:51:46 +0300

    AndroidPlatformServices: override GoogleLoginService
    Thanks @alviteri and @jamuir

--- modules/AndroidPlatformServices/Android.mk
+++ modules/AndroidPlatformServices/Android.mk
@@ -3,5 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := AndroidPlatformServices
 LOCAL_PACKAGE_NAME := com.google.android.gms.policy_sidecar_o
+GAPPS_LOCAL_OVERRIDES_PACKAGES := GoogleLoginService
 
 include $(BUILD_GAPPS_PREBUILT_APK)
