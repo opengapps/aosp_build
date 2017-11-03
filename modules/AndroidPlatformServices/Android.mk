7332fe598f1516d7454cf7e7f10c0fec07462433
Tomasz Witke <kyvaith@gmail.com>
2017-11-03 05:32:03 -0700

    Add missing signing rules

--- modules/AndroidPlatformServices/Android.mk
+++ modules/AndroidPlatformServices/Android.mk
@@ -2,6 +2,7 @@ ifneq ($(filter arm64, $(TARGET_ARCH)),)
 LOCAL_PATH := .
 include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
+LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE := AndroidPlatformServices
 LOCAL_PACKAGE_NAME := com.google.android.gms.policy_sidecar_o
 GAPPS_LOCAL_OVERRIDES_PACKAGES := GoogleLoginService
