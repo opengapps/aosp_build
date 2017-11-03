ac52a3a116f9a428496a7ca9cbebe82ddc18dad2
Tomasz Witke <kyvaith@gmail.com>
2017-11-03 06:10:57 -0700

    No need to include old BugReportSender

--- modules/BugReportSender/Android.mk
+++ modules/BugReportSender/Android.mk
@@ -3,6 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE := BugReportSender
-LOCAL_PACKAGE_NAME := com.google.tungsten.bugreportsender
+LOCAL_PACKAGE_NAME := com.google.android.tv.bugreportsender
 include $(BUILD_GAPPS_PREBUILT_APK)
 
--- modules/BugReportSenderTV/Android.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := .
-include $(CLEAR_VARS)
-include $(GAPPS_CLEAR_VARS)
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE := BugReportSenderTV
-LOCAL_PACKAGE_NAME := com.google.android.tv.bugreportsender
-include $(BUILD_GAPPS_PREBUILT_APK)
-
