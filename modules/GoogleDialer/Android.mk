2bf1b9c095ab0d230a8a2001c81028d72cd89ecc
Arne-Christian Blystad <arne.christian@blystad.me>
2018-02-19 14:22:20 +0100

    Stop including Nexus/Pixel sysconfig xmls on all devices
    This fixes issues where any device building with OpenGapps would
    believe it's a Google Pixel device.
    
    Fixes issue #134

--- modules/GoogleDialer/Android.mk
+++ modules/GoogleDialer/Android.mk
@@ -9,3 +9,15 @@ GAPPS_LOCAL_OVERRIDES_MIN_VARIANT :=
 GAPPS_LOCAL_OVERRIDES_PACKAGES := Dialer
 
 include $(BUILD_GAPPS_PREBUILT_APK)
+
+include $(CLEAR_VARS)
+include $(GAPPS_CLEAR_VARS)
+
+LOCAL_MODULE := GoogleDialer_dialer_experience.xml
+LOCAL_MODULE_STEM := dialer_experience.xml
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := $(GAPPS_SOURCES_PATH)/all/etc/sysconfig/dialer_experience.xml
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/sysconfig
+
+include $(BUILD_PREBUILT)
