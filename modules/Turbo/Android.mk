afdb711a618146b7e30a9b1ad8d5ea072d280731
James Muir <james.muir@graphitesoftware.com>
2018-07-17 18:44:28 -0400

    Install Turbo.apk under /system/priv-app
    This is what the opengapps install script does.

--- modules/Turbo/Android.mk
+++ modules/Turbo/Android.mk
@@ -3,5 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := Turbo
 LOCAL_PACKAGE_NAME := com.google.android.apps.turbo
+LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_GAPPS_PREBUILT_APK)
