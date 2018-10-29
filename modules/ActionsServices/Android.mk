063c4c9bd17a853339565e4b0d1c7015f1a5e54a
tsubus <tsubus@ilwt.org>
2018-10-29 14:57:37 +0100

    Add ActionsServices

---
+++ modules/ActionsServices/Android.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := .
+include $(CLEAR_VARS)
+include $(GAPPS_CLEAR_VARS)
+LOCAL_MODULE := ActionsServices
+LOCAL_PACKAGE_NAME := com.google.android.as
+
+include $(BUILD_GAPPS_PREBUILT_APK)
