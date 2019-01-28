ad7e4a7a6a3deebceb2e0f17ce647a7bfc19618a
Danny Baumann <dannybaumann@web.de>
2019-01-28 16:05:25 +0100

    Install keyboard swipe lib for nano+ packages.

--- modules/LatinImeGoogle/Android.mk
+++ modules/LatinImeGoogle/Android.mk
@@ -5,6 +5,11 @@ LOCAL_MODULE := LatinImeGoogle
 LOCAL_PACKAGE_NAME := com.google.android.inputmethod.latin
 
 GAPPS_LOCAL_OVERRIDES_MIN_VARIANT :=
-GAPPS_LOCAL_OVERRIDES_PACKAGES := LatinIME
+GAPPS_LOCAL_OVERRIDES_PACKAGES := LatinIME libjni_latinimegoogle
 
 include $(BUILD_GAPPS_PREBUILT_APK)
+
+include $(CLEAR_VARS)
+include $(GAPPS_CLEAR_VARS)
+LOCAL_MODULE := libjni_latinimegoogle
+include $(BUILD_GAPPS_PREBUILT_SHARED_LIBRARY)
