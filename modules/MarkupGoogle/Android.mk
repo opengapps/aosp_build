31ef1f98eada068d7553341b56d1872200acc67a
edward-p <edward@edward-p.xyz>
2019-06-26 20:55:13 +0800

    add libsketchology_native for Markup

--- modules/MarkupGoogle/Android.mk
+++ modules/MarkupGoogle/Android.mk
@@ -4,4 +4,10 @@ include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := MarkupGoogle
 LOCAL_PACKAGE_NAME := com.google.android.markup
 
+LOCAL_REQUIRED_MODULES := libsketchology_native
 include $(BUILD_GAPPS_PREBUILT_APK)
+
+include $(CLEAR_VARS)
+include $(GAPPS_CLEAR_VARS)
+LOCAL_MODULE := libsketchology_native
+include $(BUILD_GAPPS_PREBUILT_SHARED_LIBRARY)
