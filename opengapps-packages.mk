opengapps/aosp_build

commit 95a9e11d15bfce0e85565dfb1fa402e05b9c5f98

overlay: Move webview overlay from device/vendor to GoogleWebView RRO.
Replicate GoogleWebViewOverlay.apk that contains the webviewprovider
overlay and install it to the system partition, just like all other
Gapps.
This overlay would otherwise end up in the device-specific RRO (or built
directly into the framework when the device doesn't enforce an RRO for
framework-res) where it doesn't belong.

As far as I'm aware RRO packages have only recently been added in Oreo
(SDK 26).  A fallback for Nougat has been kept that adds the moved file
to the device overlay.

Signed-off-by: MarijnS95 <marijns95@gmail.com>

opengapps-packages.mk
@@ -168,9 +168,11 @@ PRODUCT_PACKAGES += $(filter-out $(GAPPS_EXCLUDED_PACKAGES),$(GAPPS_PRODUCT_PACK
 
 ifeq ($(GAPPS_FORCE_WEBVIEW_OVERRIDES),true)
 # starting with nougat, use a different overlay
-ifneq ($(filter 24,$(call get-allowed-api-levels)),)
+ifneq ($(filter 26,$(call get-allowed-api-levels)),)
+PRODUCT_PACKAGES += GoogleWebViewOverlay
+else ifneq ($(filter 24,$(call get-allowed-api-levels)),)
 DEVICE_PACKAGE_OVERLAYS += \
-    $(GAPPS_DEVICE_FILES_PATH)/overlay/webview/24
+    $(GAPPS_DEVICE_FILES_PATH)/modules/GoogleWebViewOverlay/res/xml/config_webview_packages.xml
 else
 DEVICE_PACKAGE_OVERLAYS += \
     $(GAPPS_DEVICE_FILES_PATH)/overlay/webview/21

modules/GoogleWebViewOverlay/Android.mk
@@ -1,0 +1,25 @@
+#
+#  Copyright 2018, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_PACKAGE_NAME := GoogleWebViewOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)

modules/GoogleWebViewOverlay/AndroidManifest.xml
@@ -1,0 +1,26 @@
+<!--
+/**
+ * Copyright (c) 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+-->
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+        package="com.google.android.overlay.googlewebview"
+        android:versionCode="1"
+        android:versionName="1.0">
+    <overlay android:targetPackage="android"
+        android:isStatic="true"
+        android:priority="0" />
+</manifest>
+

commit c37c1a8ab8700adab3019712787e2f2f2b869051

opengapps-packages: Re-add webview device overlay for SDK <= 24.
This adds back the framework-res overlay that got moved to facilitate
the "new" GoogleWebViewOverlay package, which subsequently corrupted the
path passed to DEVICE_PACKAGE_OVERLAYS. A symlink is used to retain a
single maintainable overlay file.

Inclusion of the file validated with:

aapt2 dump xmltree
out/target/product/<device>/vendor/overlay/framework-res__auto_generated_rro_vendor.apk
--file res/xml/config_webview_packages.xml

(Use out/target/product/<device>/system/framework/framework-res.apk on a
platform/device without PRODUCT_ENFORCE_RRO_TARGETS)

Fixes: 95a9e11d15bfce0e85565dfb1fa402e05b9c5f98
Signed-off-by: MarijnS95 <marijns95@gmail.com>

opengapps-packages.mk
@@ -177,7 +177,7 @@ ifneq ($(filter 26,$(call get-allowed-api-levels)),)
 PRODUCT_PACKAGES += GoogleWebViewOverlay
 else ifneq ($(filter 24,$(call get-allowed-api-levels)),)
 DEVICE_PACKAGE_OVERLAYS += \
-    $(GAPPS_DEVICE_FILES_PATH)/modules/GoogleWebViewOverlay/res/xml/config_webview_packages.xml
+    $(GAPPS_DEVICE_FILES_PATH)/overlay/webview/24
 else
 DEVICE_PACKAGE_OVERLAYS += \
     $(GAPPS_DEVICE_FILES_PATH)/overlay/webview/21

overlay/webview/24/frameworks/base/core/res/res
@@ -1,0 +1,1 @@
+../../../../../../../modules/GoogleWebViewOverlay/res