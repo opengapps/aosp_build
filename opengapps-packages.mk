opengapps/aosp_build

commit 95a9e11d15bfce0e85565dfb1fa402e05b9c5f98

overlay: Move webview overlay from device/vendor to GoogleWebView RRO.
Replicate GoogleWebViewOverlay.apk that contains the webviewprovider
overlay and install it to the system partition, just like all other
Gapps.
This overlay would otherwise end up in the device-specific RRO (or built
directly into the framework when the device doesn't enforce an RRO for
framework-res) where it doesn't belong.

As far as I'm aware RRO packages have only recently been added in Oreo
(SDK 26).  A fallback for Nougat has been kept that adds the moved file
to the device overlay.

Signed-off-by: MarijnS95 <marijns95@gmail.com>

opengapps-packages.mk
@@ -168,9 +168,11 @@ PRODUCT_PACKAGES += $(filter-out $(GAPPS_EXCLUDED_PACKAGES),$(GAPPS_PRODUCT_PACK
 
 ifeq ($(GAPPS_FORCE_WEBVIEW_OVERRIDES),true)
 # starting with nougat, use a different overlay
-ifneq ($(filter 24,$(call get-allowed-api-levels)),)
+ifneq ($(filter 26,$(call get-allowed-api-levels)),)
+PRODUCT_PACKAGES += GoogleWebViewOverlay
+else ifneq ($(filter 24,$(call get-allowed-api-levels)),)
 DEVICE_PACKAGE_OVERLAYS += \
-    $(GAPPS_DEVICE_FILES_PATH)/overlay/webview/24
+    $(GAPPS_DEVICE_FILES_PATH)/modules/GoogleWebViewOverlay/res/xml/config_webview_packages.xml
 else
 DEVICE_PACKAGE_OVERLAYS += \
     $(GAPPS_DEVICE_FILES_PATH)/overlay/webview/21

modules/GoogleWebViewOverlay/Android.mk
@@ -1,0 +1,25 @@
+#
+#  Copyright 2018, The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_PACKAGE_NAME := GoogleWebViewOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)

modules/GoogleWebViewOverlay/AndroidManifest.xml
@@ -1,0 +1,26 @@
+<!--
+/**
+ * Copyright (c) 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+-->
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+        package="com.google.android.overlay.googlewebview"
+        android:versionCode="1"
+        android:versionName="1.0">
+    <overlay android:targetPackage="android"
+        android:isStatic="true"
+        android:priority="0" />
+</manifest>
+

commit e084a04414068f88427e9fb78e524df7d340e345

packages: Use device overlay instead of GoogleWebViewOverlay on SDK<=28.
Reports from Android 9 users indicate that `dumpsys webviewupdate` does
not contain any of the webviews listed in GoogleWebViewOverlay despite
the overlay being statically enabled according to `dumpsys overlay`, and
it containing the xml file with expected nodes when inspected with
`aapt2 dump xmltree --file res/xml/config_webview_packages.xml`.

A quick check confirms that the last Android 9 crosshatch system image
includes res/xml/config_webview_packages.xml directly in
/system/framework/framework-res.apk instead of through a separate
overlay APK.
This, together with the fact that the overlay isn't properly applied
even when residing in /system/overlay/ leads to believe that the
specific <webviewproviders> xml does not get overlaid, or at least
incorrectly.

However:
DEVICE_PACKAGE_OVERLAYS causes the overlaid files to end up in an
autogenerated RRO in device trees that declare `framework-res` (or the
wildcard `*`) in PRODUCT_ENFORCE_RRO_TARGETS. The xml from this overlay
package doesn't get overlaid accordingly either.
(sidenote: This RRO file being on /vendor, whereas opengapps supposedly
 lives completely on the system partition is the reason
 GoogleWebViewOverlay exists and is placed on /system or /product).

If anyone feels like debugging, be my guest. For now this change is
reverted to the old behaviour on SDK 24-28.

Fixes: 95a9e11d15bfce0e85565dfb1fa402e05b9c5f98
Signed-off-by: MarijnS95 <marijns95@gmail.com>

opengapps-packages.mk
@@ -172,10 +172,11 @@ endif # end nano
 PRODUCT_PACKAGES += $(filter-out $(GAPPS_EXCLUDED_PACKAGES),$(GAPPS_PRODUCT_PACKAGES))
 
 ifeq ($(GAPPS_FORCE_WEBVIEW_OVERRIDES),true)
-# starting with nougat, use a different overlay
-ifneq ($(filter 26,$(call get-allowed-api-levels)),)
+ifneq ($(filter 29,$(call get-allowed-api-levels)),)
+# starting with Q, put the overlay in a product APK
 PRODUCT_PACKAGES += GoogleWebViewOverlay
 else ifneq ($(filter 24,$(call get-allowed-api-levels)),)
+# starting with nougat, use a different overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(GAPPS_DEVICE_FILES_PATH)/overlay/webview/24
 else