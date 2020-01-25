opengapps/aosp_build

commit 191c464a3fcb28b1938a47ba74713c712101c3cd

Include trichromelibrary with Chrome/WebView on SDK >= 29.
Chrome and the WebViewChrome library have all their shared code moved to
TrichromeLibrary. Add a prebuilt module for this library, which is
required to run either apk.

Without this, Chrome and anything using WebView will crash (unless you
select a different WebView or install it fresh from the Play Store).

Signed-off-by: MarijnS95 <marijns95@gmail.com>

modules/TrichromeLibrary/Android.mk
@@ -1,0 +1,7 @@
+LOCAL_PATH := .
+include $(CLEAR_VARS)
+include $(GAPPS_CLEAR_VARS)
+LOCAL_MODULE := TrichromeLibrary
+LOCAL_PACKAGE_NAME := com.google.android.trichromelibrary
+
+include $(BUILD_GAPPS_PREBUILT_APK)

modules/Chrome/Android.mk
@@ -7,4 +7,9 @@ LOCAL_PACKAGE_NAME := com.android.chrome
 GAPPS_LOCAL_OVERRIDES_MIN_VARIANT := stock
 GAPPS_LOCAL_OVERRIDES_PACKAGES := Browser Browser2 BrowserProviderProxy Chromium Fluxion Gello Jelly
 
+ifneq ($(filter 29,$(call get-allowed-api-levels)),)
+LOCAL_REQUIRED_MODULES += \
+    TrichromeLibrary
+endif
+
 include $(BUILD_GAPPS_PREBUILT_APK)

modules/WebViewGoogle/Android.mk
@@ -9,4 +9,9 @@ LOCAL_OVERRIDES_PACKAGES := webview
 LOCAL_REQUIRED_MODULES := libwebviewchromium_loader \
                           libwebviewchromium_plat_support
 
+ifneq ($(filter 29,$(call get-allowed-api-levels)),)
+LOCAL_REQUIRED_MODULES += \
+    TrichromeLibrary
+endif
+
 include $(BUILD_GAPPS_PREBUILT_APK)

commit 946eadb086e6335e6721c18fe8c1d1a9c2d68433

modules: Load TrichromeLibrary module only on API >= 29.
The APK for this module only exists on API level 29. Builds for Android
9 and below will fail due to the missing APK on a lower or matching API
level.
Besides, this module is never added to LOCAL_REQUIRED_MODULES on API
levels lower than 29 anyway.

Fixes: 191c464a3fcb28b1938a47ba74713c712101c3cd
Signed-off-by: MarijnS95 <marijns95@gmail.com>

modules/TrichromeLibrary/Android.mk
@@ -1,3 +1,5 @@
+ifneq ($(filter 29,$(call get-allowed-api-levels)),)
+
 LOCAL_PATH := .
 include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
@@ -5,3 +7,5 @@ LOCAL_MODULE := TrichromeLibrary
 LOCAL_PACKAGE_NAME := com.google.android.trichromelibrary
 
 include $(BUILD_GAPPS_PREBUILT_APK)
+
+endif # API >= 29