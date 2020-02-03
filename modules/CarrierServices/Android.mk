8feb52d996d2d9b3e53a04b4fdbf4902a5ab2880
Angga Satya Putra <anggasp@hotmail.com>
2020-02-03 15:20:16 +0800

    opengapps-packages: Make appropriate apps a privilege module

--- modules/CarrierServices/Android.mk
+++ modules/CarrierServices/Android.mk
@@ -3,5 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := CarrierServices
 LOCAL_PACKAGE_NAME := com.google.android.ims
+LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_GAPPS_PREBUILT_APK)
--- modules/StorageManagerGoogle/Android.mk
+++ modules/StorageManagerGoogle/Android.mk
@@ -3,5 +3,6 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := StorageManagerGoogle
 LOCAL_PACKAGE_NAME := com.google.android.storagemanager
+LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_GAPPS_PREBUILT_APK)
