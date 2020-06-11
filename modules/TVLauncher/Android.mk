49285073ebdcb680b771dd3d6807148c238bf3ef
Aaron Kling <webgeek1234@gmail.com>
2020-06-11 12:57:09 -0500

    Override tvlauncher no-gms variants
    This is the naming used by Lineage OS for the no-gms builds of
    tvlauncher and tvrecommendations which Google released for aosp.

--- modules/TVLauncher/Android.mk
+++ modules/TVLauncher/Android.mk
@@ -3,6 +3,7 @@ include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := TVLauncher
 LOCAL_PACKAGE_NAME := com.google.android.tvlauncher.leanback
+GAPPS_LOCAL_OVERRIDES_PACKAGES := TVLauncherNoGMS
 LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_GAPPS_PREBUILT_APK)
