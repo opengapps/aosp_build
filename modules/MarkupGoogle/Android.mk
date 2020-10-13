4e79c103102f789852e80d17c91f8c812c55911c
Pablo Mendez Hernandez <pablomh@gmail.com>
2020-10-13 15:59:17 +0200

    modules: MarkupGoogle: Provide LOCAL_SHARED_LIBRARIES
    Do it to satisfy check_elf_file.py requirements.
    
    Signed-off-by: Pablo Mendez Hernandez <pablomh@gmail.com>

--- modules/MarkupGoogle/Android.mk
+++ modules/MarkupGoogle/Android.mk
@@ -10,4 +10,5 @@ include $(BUILD_GAPPS_PREBUILT_APK)
 include $(CLEAR_VARS)
 include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := libsketchology_native
+LOCAL_SHARED_LIBRARIES := libEGL libGLESv1_CM libGLESv2 libandroid libjnigraphics liblog
 include $(BUILD_GAPPS_PREBUILT_SHARED_LIBRARY)
